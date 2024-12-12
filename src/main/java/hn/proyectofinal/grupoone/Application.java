a621ceda91c593f445eecc0ae2324f3e261b1b71
carlosenamorado <cdenamorado1@uth.hn>
2024-12-11 22:02:11 -0600

    pruebadeTexto

--- src/main/java/hn/proyectofinal/grupoone/Application.java
+++ src/main/java/hn/proyectofinal/grupoone/Application.java
@@ -22,4 +22,4 @@ public class Application implements AppShellConfigurator {
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
     }
-}
+} //*pRUEBA DE TEXTO
